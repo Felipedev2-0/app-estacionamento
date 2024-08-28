fde957c4675cfa769b1738e989d7fbe2ea115387
Felipe Barros <filbarros@hotmail.com>
2024-08-28 20:35:02 -0300

    app-estacionamento - inicio

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <p>Sistema de Estacionamento - 2024 - Coffe Tag</p>
+  
+</template>
+
+<script>
+export default {
+    name: 
+        "Footer"
+    
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
+
